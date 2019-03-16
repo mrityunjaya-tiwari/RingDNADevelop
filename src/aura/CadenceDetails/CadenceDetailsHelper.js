172c7f6f01d2a409ad388cd016738ac0e90f4660
PANKAJ-SHARMA1\Pankaj <pankaj.sharma1@metacube.com>
2019-03-16 12:41:23 +0530

    remove commented code

--- src/aura/CadenceDetails/CadenceDetailsHelper.js
+++ src/aura/CadenceDetails/CadenceDetailsHelper.js
@@ -4,11 +4,6 @@
     },
     fireRecordTypeChangedEvent: function(component, event, helper) {
         component.set('v.settedSequenceType', true);
-        /*var appEvent = $A.get("e.RDNACadence4:RecordTypeChangeEvent");
-        var cadence = component.get("v.newCadence");
-        appEvent.setParams({
-            "recordType" :  cadence.RDNACadence4__Record_Type__c});
-        appEvent.fire(); */
     }, 
     createListForInput : function (component){
         var fieldList = component.get("c.createListForUIInput");
