1c92a2eb106f1f4bdee6b7fa1bfcca7da17f4694
shikha_verma <shikha.verma@metacube.com>
2019-04-26 11:48:25 +0530

    Code committed for feature/SFDC-460

--- src/aura/ParticipantsList/ParticipantsListController.js
+++ src/aura/ParticipantsList/ParticipantsListController.js
@@ -1,18 +1,15 @@
 ({
 	doInit : function(component, event, helper) {
-        var noOFColumns = 5;
-        var  width = screen.width/noOFColumns;
-        width = width -31.2;
         component.set('v.columns', [ 
-            { label: 'Name', initialWidth: width,fieldName: 'linkName', type: 'url', sortable:'true', 
+            { label: 'Name', fieldName: 'linkName', type: 'url', sortable:'true', 
             	typeAttributes: {label: { fieldName: 'name' }, tooltip: {fieldName: 'name'}, target: '_self', sortable: true}},
             {label: 'Company', fieldName: 'linkcompany', type: 'url', sortable:'true',
              typeAttributes: {label: { fieldName: 'company' }, target: '_self', sortable: true}, cellAttributes: 
              {class: 'ringdna-company-td'}
             },
-            { label: 'Status', initialWidth: width,  fieldName: 'status', type: 'text', sortable: true},
-            { label: 'Email', initialWidth:width, fieldName: 'email', type: 'email', sortable: true},
-            { label: 'Phone', initialWidth: width, fieldName: 'phone', type: 'phone', sortable: true, cellAttributes:
+            { label: 'Status', fieldName: 'status', type: 'text', sortable: true},
+            { label: 'Email', fieldName: 'email', type: 'email', sortable: true},
+            { label: 'Phone', fieldName: 'phone', type: 'phone', sortable: true, cellAttributes:
                 { class: 'ringdna-phone-td'}}
         ]);
 
