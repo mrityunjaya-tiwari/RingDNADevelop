269b9359a98ed7bbf35f4a99a5260c3b68405a8f
shikha_verma <shikha.verma@metacube.com>
2019-05-17 14:59:23 +0530

    code committed for duplicate SFDC-556

--- src/aura/ManualSequenceAssociate/ManualSequenceAssociateHelper.js
+++ src/aura/ManualSequenceAssociate/ManualSequenceAssociateHelper.js
@@ -73,9 +73,10 @@
             var state = response.getState();
             if(state === "SUCCESS") {
                 var errorRecords = response.getReturnValue();
-                if(errorRecords[0]>0 || errorRecords[1]>0){
+                if(errorRecords[0]>0 || errorRecords[1]>0 || errorRecords[2]>0){
                     cmp.set('v.noOfRecordsWithActiveSequence', errorRecords[0]);
                     cmp.set('v.noOfRecordsWithSameSequencePreviously', errorRecords[1]);
+                    cmp.set('v.noOfRecordsMatchingExitCriteria', errorRecords[2]);
                     cmp.set('v.showModalMessage', true);
                 }else{
                     helper.redirectToPreviousView(cmp, event, helper);
